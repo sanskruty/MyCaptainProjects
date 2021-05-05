ac4db0c92a65bbbf9ed1f0a7bef3eaa26a95284a
sanskruty <47455602+sanskruty@users.noreply.github.com>
2021-05-05 12:30:31 +0530

    Task 2 of Coding loops
    A program to print all the positive numbers from a list of numbers.

---
+++ positives.py
@@ -0,0 +1,16 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+
+ot_li1 = []
+print("List 1 positives: ")
+for i in list1:
+    if i > 0:
+        ot_li1.append(i)
+print(ot_li1)
+
+ot_li2 = []
+print("List 2 positives: ")
+for i in list2:
+    if i > 0:
+        ot_li2.append(i)
+print(ot_li2)
