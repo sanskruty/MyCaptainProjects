40d4e14606d4691729bb79a4c5c012d93eb3944f
sanskruty <47455602+sanskruty@users.noreply.github.com>
2021-05-05 12:20:24 +0530

    Update Fibonnaci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -3,7 +3,7 @@ number_of_iterations = int(input("How many iteration of fibonacci do you want?\t
 a = 0
 b = 1
 print(a, b, end=" ")
-for iter in range(1, number_of_iterations):
+for iter in range(1, number_of_iterations-1):
     c = a + b
     a = b
     b = c
